e2eba4e572457d96455a1b5f2995754193368350
Gerald Yeo <gerald.yeo@gmail.com>
2016-01-11 19:26:49 +0800

    linting setup

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,5 @@ import Route from 'react-router';
 import App from './containers/App';
 
 export default (
-	<Route component={App}>
-	</Route>
+	<Route component={App} />
 );
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -8,68 +8,74 @@ var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./w
 
 var assetsPath = path.resolve(__dirname, '../static/dist');
 var host = (process.env.HOST || 'localhost');
-var port = parseInt(process.env.PORT) + 1 || 9001;
+var port = parseInt(process.env.PORT, 10) + 1 || 9001;
 var hotMiddlewareScript = 'webpack-hot-middleware/client?path=http://' + host + ':' + port + '/__webpack_hmr';
 
 var commonLoaders = [
-  {
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    loader: 'babel',
-    query: {
-      presets: ['react-hmre']
-    }
-  },
-  {
-    test: /\.css$/,
-    loaders: [
-      'style-loader',
-      'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]',
-      'postcss-loader'
-    ]
-  },
-  { 
-    test: webpackIsomorphicToolsPlugin.regular_expression('images'),
-    loader: 'url-loader?limit=10240'
-  }
+	{
+		test: /\.(js|jsx)$/,
+		exclude: /node_modules/,
+		loader: 'babel',
+		query: {
+			presets: ['react-hmre']
+		}
+	},
+	{
+		test: /\.css$/,
+		loaders: [
+			'style-loader',
+			'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]',
+			'postcss-loader'
+		]
+	},
+	{
+		test: webpackIsomorphicToolsPlugin.regular_expression('images'),
+		loader: 'url-loader?limit=10240'
+	}
 ];
 
 module.exports = {
-  devtool: 'inline-source-map',
-  context: path.resolve(__dirname, '..'),
-  entry: {
-    main: ['./app/client.js', hotMiddlewareScript]
-  },
-  output: {
-    path: assetsPath,
-    filename: '[name]-[hash].js',
-    chunkFilename: '[name]-[chunkhash].js',
-    publicPath: 'http://' + host + ':' + port + '/dist/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify('development') },
-      __CLIENT__: true,
-      __SERVER__: false,
-      __PRODUCTION__: false,
-      __DEVELOPMENT__: true,
-      __DEVELOPMENT_TOOLS__: false
-    }),
-    webpackIsomorphicToolsPlugin.development()
-  ],
-  module: {
-    loaders: commonLoaders.concat()
-  },
-  progress: true,
-  resolve: {
-    extensions: ['', '.json', '.js', '.jsx'],
-    modulesDirectories: ['node_modules']
-  },
-  postcss: [
-    rucksack({
-      autoprefixer: true
-    })
-  ]
+	devtool: 'inline-source-map',
+	context: path.resolve(__dirname, '..'),
+	entry: {
+		main: ['./app/client.js', hotMiddlewareScript]
+	},
+	output: {
+		path: assetsPath,
+		filename: '[name]-[hash].js',
+		chunkFilename: '[name]-[chunkhash].js',
+		publicPath: 'http://' + host + ':' + port + '/dist/'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': { NODE_ENV: JSON.stringify('development') },
+			__CLIENT__: true,
+			__SERVER__: false,
+			__PRODUCTION__: false,
+			__DEVELOPMENT__: true,
+			__DEVELOPMENT_TOOLS__: false
+		}),
+		webpackIsomorphicToolsPlugin.development()
+	],
+	module: {
+		preLoaders: [
+			{ test: /\.(js|jsx)$/, loader: 'eslint-loader', exclude: /node_modules/ }
+		],
+		loaders: commonLoaders.concat()
+	},
+	progress: true,
+	resolve: {
+		extensions: ['', '.json', '.js', '.jsx'],
+		modulesDirectories: ['node_modules']
+	},
+	postcss: [
+		rucksack({
+			autoprefixer: true
+		})
+	],
+	eslint: {
+		configFile: path.resolve(__dirname, '../.eslintrc')
+	}
 };
