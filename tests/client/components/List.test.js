9b873ce15547ed569e7986f159babd452412a249
Gerald Yeo <gerald.yeo@gmail.com>
2016-01-20 16:31:28 +0800

    example for using mjackson's expect lib

--- tests/client/components/List.test.js
+++ tests/client/components/List.test.js
@@ -1,8 +1,15 @@
+import tape from 'tape';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { renderIntoDocument } from 'react-addons-test-utils';
 import expect, { createSpy, spyOn, isSpy } from 'expect';
-import tape from 'tape';
+
+tape('it should throw', (test) => {
+	test.doesNotThrow(() => {
+		expect(1).toEqual(1);
+	});
+	test.end();
+});
 
 tape('it should fail', (test) => {
 	test.equal(false, true, 'it should fail miserably.');
--- tests/server/actions/users.test.js
+++ tests/server/actions/users.test.js
@@ -1,5 +1,4 @@
-import expect, { createSpy, spyOn, isSpy } from 'expect';
-import tape from 'tape-catch';
+import tape from 'tape';
 
 tape('Users', (test) => {
 	test.equal(true, false, 'it should fail too');
